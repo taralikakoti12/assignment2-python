3a8ed587ed6cd566ed48128784ee717a51924597
taralikakoti12 <taraleekakoty007@gmail.com>
2025-05-29 10:54:18 +0530

    Add files via upload

---
+++ basic/add two integers entered by user.py
@@ -0,0 +1,4 @@
+num1=int(input("Enter the integer1:"))
+num2=int(input("Enter the integer2"))
+sum=num1+num2
+print("The sum is:",sum)
---
+++ basic/integer entered by user.py
@@ -0,0 +1,2 @@
+num=int(input("Enter a number:"))
+print("The number entered is:",num)
