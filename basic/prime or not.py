d165c237d1bffd7f028d917f36501099f4c2ab16
taralikakoti12 <taraleekakoty007@gmail.com>
2025-05-29 10:52:55 +0530

    Delete basic/prime or not.py

--- basic/prime or not.py
+++
@@ -1,9 +0,0 @@
-num=int(input("Enter a number:"))
-if num<=1:
-    print(False)
-else:
-    is_prime=True
-    for i in range(2,num+1):
-        is_prime=False
-        break
-    print("The number is prime:"is_prime)
