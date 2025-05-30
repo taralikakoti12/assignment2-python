11f56e5088b854592290c6ca1879e048f3652a69
taralikakoti12 <taraleekakoty007@gmail.com>
2025-05-30 21:56:08 +0530

    Add files via upload

---
+++ if else/quadratic equation.py
@@ -0,0 +1,8 @@
+import cmath
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+c=int(input("Enter the value of c:"))
+d=(b**2)-(4*a*c)
+sol1=(-b-cmath.sqrt(d))/(2*a)
+sol2=(-b+cmath.sqrt(d))/(2*a)
+print("The solution are",sol1 "and",sol2)
